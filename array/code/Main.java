b4bada0b4d65d2263b6ed4f9f5983e54ce2e8d9b
Naman85Jain <jainnaman8591@gmail.com>
2025-12-09 09:37:50 +0530

    cleanup

---
+++ array/code/Main.java
@@ -0,0 +1,7 @@
+package code;
+// An array is a collection of items of the same variable type that are stored at contiguous memory 
+// locations. It is one of the most popular and simple data structures used in programming.
+
+public class Main {
+    
+}
